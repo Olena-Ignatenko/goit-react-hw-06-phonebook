302079332cb3c7dba9904a162c2494281e443f56
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2024-04-01 01:05:07 +0300

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,17 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 const ContactList = ({  onDelete }) => {
   const contactsData = useSelector((state) => state.contacts);
+  const search = useSelector(selectNameFilter);
+  
+   const filterContacts = contactsData.filter((contact) =>
+     contact.name.toLowerCase().includes(search.trim().toLowerCase())
+   );
   return (
     <ul>
-      {contactsData.map((contact) => (
+      {filterContacts.map((contact) => (
         <Contact key={contact.id} contact={contact} onDelete={onDelete} />
       ))}
     </ul>
