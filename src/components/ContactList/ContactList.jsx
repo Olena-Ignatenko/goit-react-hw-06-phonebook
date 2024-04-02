7e0b05355fe83a6d6deffe97e024e191ca82c735
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2024-04-02 22:33:03 +0300

    update ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,16 +7,20 @@ const ContactList = () => {
   const contactsData = useSelector(selectContacts);
   const search = useSelector(selectNameFilter);
   
-   const filterContacts = contactsData.filter((contact) =>
-     contact.name.toLowerCase().includes(search.trim().toLowerCase())
-   );
-  return (
-    <ul>
-      {filterContacts.map((contact) => (
-        <Contact key={contact.id} contact={contact}  />
-      ))}
-    </ul>
-  );
-};
-
+  let filterContacts = [];
+  //  const filterContacts = contactsData.filter((contact) =>
+  if (contactsData !== undefined) { // Перевірка на undefined
+    filterContacts = contactsData.filter((contact) =>
+      contact.name.toLowerCase().includes(search.trim().toLowerCase())
+    );
+ 
+    return (
+      <ul>
+        {filterContacts.map((contact) => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </ul>
+    );
+  }
+}
 export default ContactList;
